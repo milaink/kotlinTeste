c2530e2575117a63bda66ae83f6934c2936c1fb8
Camila M <camilaom89@gmail.com>
2022-03-19 00:32:42 -0300

    Testando Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Bem vindo ao ByteBank!")
+    val titular: String = "Alex"
+    val numeroConta: Int = 1253
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200.0
+
+    //variáveis: mutáveis (var) imutáveis (val)
+    println("O número da conta é $numeroConta")
+    println("Titular $titular")
+    println("Saldo: $saldo")
+
+}
